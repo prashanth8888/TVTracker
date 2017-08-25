8b77a93c39ce7fb0f258a69168ef6f36981dfb37
Prashanth Seralathan <prashanth.s1991@hotmail.com>
2017-08-25 20:24:26 +0000

    Basic Show page - Still need to figure out about resolving promises

--- trackerapp/public/controllers/showController.js
+++ trackerapp/public/controllers/showController.js
@@ -2,10 +2,24 @@ var app = angular.module('MyApp');
 
 app.controller('ShowCtrl',['$scope','$routeParams','Detail', function($scope, $routeParams, Detail) {
 
+    $scope.seasonsData  = {};
+    $scope.currentSeason = null;
+    $scope.episodesData = [];
+    $scope.currentSeasonEpisodesData = {};
+    
+    
     $scope.getShow = function(showId){
         Detail.displaybyShowId(showId).then(function(response){
-            console.log("Sesons Data " + response.data.seasonsData);
-            console.log("Episodes Data " + response.data.episodesData);
+            $scope.seasonsData = {};
+            $scope.episodesData.length = 0;
+            for(var i = 0; i < response.data.episodesData.length; i++){
+                if(i == 0)
+                   $scope.currentSeason = i;
+                $scope.episodesData.push(response.data.episodesData[i]);
+            }
+            $scope.currentSeasonEpisodesData = $scope.episodesData[$scope.currentSeason];
+            $scope.seasonsData = response.data.seasonsData;
+            console.log($scope.seasonsData);
         }, function(){
             console.log("Error while trying to build Show details page");
         });
